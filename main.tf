11d884446dbbe8c57c833371b8d616d25d6c016d
Ppatil <ppatil@nextgen.com>
2023-06-06 12:47:32 +0530

    fromlocal

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_security_group" "allow_port80" {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = [aws_vpc.ecommerce-vpc.ipv6_cidr_block]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
