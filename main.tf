6b33c062b96da643bb612a069bbe02f47e229c6f
root <root@ip-172-31-25-38.ec2.internal>
2023-06-06 06:46:48 +0000

    maintffromaws

--- main.tf
+++ main.tf
@@ -15,15 +15,80 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-    ami = "ami-0851b76e8b1bce90b"
-    instace_type = "t2.micro"
+resource "aws_vpc" "ecommerce-vpc"{
+    cidr_block = "10.10.0.0/16"
+}
 
+resource "aws_subnet" "subnet-1a"{
+    vpc_id = aws_vpc.ecommerce-vpc.id
+    cidr_block = "10.10.1.0/24"
+    availability_zone = "ap-south-1a"
     tags = {
-        Name = "Machine1FromTerraform"
+	Name = "subnet-1a"
     }
 }
 
-resource "aws_eip""app_ipaddress"{
-    
+
+resource "aws_subnet" "subnet-1b"{
+    vpc_id = aws_vpc.ecommerce-vpc.id
+    cidr_block = "10.10.2.0/24"
+    availability_zone = "ap-south-1b"
+    tags = {
+	Name = "subnet-1b"
+    }
 }
+
+
+resource "aws_subnet" "subnet-1c"{
+    vpc_id = aws_vpc.ecommerce-vpc.id
+    cidr_block = "10.10.3.0/24"
+    availability_zone = "ap-south-1c"
+    tags = {
+	Name = "subnet-1c"
+    }
+}
+
+resource "aws_instance" "web001" {
+#    ami = "ami-0851b76e8b1bce90b"
+    ami = "ami-000051d5c1a3d7008"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "web001"
+	App = "wordpress"
+#	Webserver = "Nginx"
+    }
+    subnet_id = aws_subnet.subnet-1a.id
+}
+
+
+resource "aws_instance" "web002" {
+#    ami = "ami-0851b76e8b1bce90b"
+    ami = "ami-000051d5c1a3d7008"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "web002"
+	App = "wordpress"
+#	Webserver = "Nginx"
+    }
+    subnet_id = aws_subnet.subnet-1b.id
+}
+
+
+resource "aws_instance" "web003" {
+#    ami = "ami-0851b76e8b1bce90b"
+    ami = "ami-000051d5c1a3d7008"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "web003"
+	App = "wordpress"
+#	Webserver = "Nginx"
+    }
+    subnet_id = aws_subnet.subnet-1c.id
+}
+
+#resource "aws_eip""app_ipaddress"{
+    
+#}
