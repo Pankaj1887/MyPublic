1543ea7b37cf20e5674babf1493d03bbce41c4ae
Ppatil <ppatil@nextgen.com>
2023-06-06 12:27:16 +0530

    fromlocal

--- main.tf
+++ main.tf
@@ -92,3 +92,30 @@ resource "aws_instance" "web003" {
 #resource "aws_eip""app_ipaddress"{
     
 #}
+
+resource "aws_security_group" "allow_port80" {
+  name        = "allow_port80"
+  description = "allow_port80 inbound traffic"
+  vpc_id      = aws_vpc.ecommerce-vpc.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = "0.0.0.0/0"
+    ipv6_cidr_blocks = [aws_vpc.ecommerce-vpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
