ba99df11f2b43f0456a6ce901d1b8fb4dbc7c15f
Ppatil <ppatil@nextgen.com>
2023-06-15 11:39:46 +0530

    fromlocal

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_subnet" "subnet-1b"{
     vpc_id = aws_vpc.ecommerce-vpc.id
     cidr_block = "10.10.2.0/24"
     availability_zone = "ap-south-1b"
+    map_public_ip_on_launch = "true"
     tags = {
 	Name = "subnet-1b"
     }
