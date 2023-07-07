576051ed78f7323c6df416804cece54596b2c3ef
Ppatil <ppatil@nextgen.com>
2023-07-07 18:02:50 +0530

    fromlocal

--- customer/main.tf
+++ customer/main.tf
@@ -24,3 +24,15 @@ module "vpc1" {
     name = "vpc-prod1"
 
 }
+
+
+module "subnet1" {
+    source = "git::https://github.com/Pankaj1887/MyPublic.git//tfmodules/subnet"
+    subnet_cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = false
+    vpc_id = module.vpc1.id
+    environment = "env1"
+    product = "prod1"
+    name = "subnet-prod1"
+
+}
---
+++ tfmodules/subnet/variable.tf
@@ -0,0 +1,23 @@
+variable "vpc_id" {
+    description = "vpc_id"
+}
+
+variable "subnet_cidr_block" {
+    description = "subnet_cidr_block"
+}
+
+variable "map_public_ip_on_launch" {
+    description = "map_public_ip_on_launch"
+}
+
+variable "name" {
+    description = "name of the VPC"
+}
+
+variable "environment" {
+    description = "environment"
+}
+
+variable "product" {
+    description = "product"
+}
