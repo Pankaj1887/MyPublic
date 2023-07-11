cec451ad4245681be2834813f6cc19341c65a6d5
Ppatil <ppatil@nextgen.com>
2023-07-11 14:50:41 +0530

    fromlocal

--- tfmodules/route_table_association1/variable.tf
+++ tfmodules/route_table_association1/variable.tf
@@ -6,3 +6,14 @@ variable "route_table_id" {
     description = "route_table_id"
 }
 
+variable "name" {
+    description = "name of the VPC"
+}
+
+variable "environment" {
+    description = "environment"
+}
+
+variable "product" {
+    description = "product"
+}
