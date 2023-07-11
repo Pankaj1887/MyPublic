4d3af11167bfc7a74092f4917c498efc3e15d46e
Ppatil <ppatil@nextgen.com>
2023-07-11 14:58:42 +0530

    fromlocal

--- tfmodules/route_table_association1/variable.tf
+++ tfmodules/route_table_association1/variable.tf
@@ -6,14 +6,4 @@ variable "route_table_id" {
     description = "route_table_id"
 }
 
-variable "name" {
-    description = "name of the VPC"
-}
 
-variable "environment" {
-    description = "environment"
-}
-
-variable "product" {
-    description = "product"
-}
