c007c03a9d64f6404061e654f662109e95126944
Ppatil <ppatil@nextgen.com>
2023-07-25 13:01:27 +0530

    fromlocal

--- tfmodules/windows_vm/outputs.tf
+++ tfmodules/windows_vm/outputs.tf
@@ -1,7 +1,7 @@
 output "id" {
-    value = aws_instance.web001.id
+    value = aws_instance.win001.id
 }
 
 output "arn" {
-    value = aws_instance.web001.arn
+    value = aws_instance.win001.arn
 }
