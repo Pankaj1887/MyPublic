69d0dcab61fd5b4da1ecb95f22b15b3472cb1265
Ppatil <ppatil@nextgen.com>
2023-08-23 12:04:13 +0530

    added autoscaling group

--- main.tf
+++ main.tf
@@ -241,3 +241,39 @@ resource "aws_lb_listener" "front_end_80" {
     target_group_arn = aws_lb_target_group.mywebservergroup.arn
   }
 }
+
+
+resource "aws_launch_configuration" "ecommerce-launch-config" {
+  name          = "ecommerce-laucch-config"
+  image_id      = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "ecommerce-asg" {
+  name                      = "ecommerce-asg"
+  max_size                  = 5
+  min_size                  = 2
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 4
+  force_delete              = true
+  placement_group           = aws_placement_group.test.id
+  launch_configuration      = aws_launch_configuration.ecommerce-launch-config.name
+  vpc_zone_identifier       = [aws_subnet.subnet-1a.id, aws_subnet.subnet-1b.id]
+
+  tag {
+    key                 = "foo"
+    value               = "bar"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+
+}
+
+resource "aws_autoscaling_attachment" "ecommerce-asg-attachment" {
+  autoscaling_group_name = aws_autoscaling_group.ecommerce-asg.id
+  lb_target_group_arn    = aws_lb_target_group.mywebservergroup.arn
+}
